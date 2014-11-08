01024f10698e983afe7fcf9fafe15d194aec538c
michelspirlandeli <michelspirlandeli_@hotmail.com>
2014-11-08 15:57:12 +0000

    michel commit4

--- projeto_Boletim/usuarios/models.py
+++ projeto_Boletim/usuarios/models.py
@@ -3,12 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Usuario(AbstractUser):
-  #username = models.CharField(max_length=50,blank=True,null= True)
-  #email = models.EmailField()
-  #password = forms.CharField(widget=forms.PasswordInput)
-  endereco = models.CharField(max_length=100, blank=True, null = True)
-  
-
-  
-
-# Create your models here.
+    endereco = models.CharField(max_length=100, blank=True, null = True)
--- projeto_Boletim/usuarios/views.py
+++ projeto_Boletim/usuarios/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render, HttpResponseRedirect
 from usuarios.forms import UsuarioForm, LoginForm
 from django.contrib.auth import authenticate, logout, login as meu_login
 from django.contrib.auth.decorators import login_required
+from usuarios.models import Usuario
+from django.core.mail import send_mail
+from django.utils.translation import ugettext_lazy as _
 
 def index(request):
     return render(request, 'index.html')
 
 def login(request):
-  form = LoginForm()
-  return render(request, 'login.html', {'form' : form})
+    form = LoginForm()
+    return render(request, 'login.html', {'form' : form})
 
 def validar(request):
-  
     if request.method == 'POST':
         form = LoginForm(request.POST)
         
@@ -19,7 +21,7 @@ def validar(request):
             usuario = authenticate(username=form.data['login'], password=form.data['senha'])
             
             if usuario is not None:
-              if usuario.is_active:
+                  if usuario.is_active:
                     meu_login(request, usuario)
                     return HttpResponseRedirect('/dashboard/')
                   else:
@@ -30,11 +32,7 @@ def validar(request):
                 return render(request, 'login.html',{'form': form})      
     else:
             return HttpResponseRedirect('/login/')
-           # usuario.save()
-            
-            #usuarios = Usuario.objects.all()
-            
-            #return render(request, 'validar.html', {'form' : form, 'usuarios' : usuarios })
+          
 def logoff(request):
     logout(request)
     return HttpResponseRedirect('/')
@@ -42,3 +40,33 @@ def logoff(request):
 @login_required()
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def cadastro(request):
+    form = cadastroForm()
+    return render(request, 'cadastro.html',{'form':form})
+
+def cadastro_validar(request):
+    if request.method == 'POST':
+        form = cadastroForm(request.POST)
+        
+        if form.is_valid():
+            usuario = Usuario(
+            username=form.data['login'],
+            email=form.data['email'],
+            is_active=False
+        )
+        usuario.set_password(form.data['senha'])
+        usuario.save()
+        
+        if send_mail('Campo assunto Aqui', 'Valide o seu email: http://unifran-78230.sae1.nitrousbox.com/token/'+str(usuario.pk), 'testeunifran@bol.com.br',
+   [usuario.email], fail_silently=True):
+            # Se der erro ele avisa aqui.
+            # Retornar tela de sucesso! Vc Consegue fazer e tb arrumar esses codigos?
+            return render(request,'cadastro.html',{'form':form})
+        return render(request,'index.html',{'form':form})
+
+def token(request, numero):
+    usuario = Pessoa.objects.get(pk=numero)
+    usuario.is_active = True
+    usuario.save()
+    return HttpResponseRedirect('/login/')
