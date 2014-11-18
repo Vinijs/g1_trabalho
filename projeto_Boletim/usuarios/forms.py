897852d78ddf27aca6727e84237417dd5795e4f1
Vinijs <vini-spider-6@hotmail.com>
2014-11-18 23:44:09 +0000

    Decimo Terceiro commit 18/11/2014

--- projeto_Boletim/usuarios/forms.py
+++ projeto_Boletim/usuarios/forms.py
@@ -16,4 +16,6 @@ class cadastroForm(forms.Form):
     email = forms.CharField(max_length='100', required=True)
     endereco = forms.CharField(max_length='25', required=True)
     
+    
+    
           
